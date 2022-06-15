73a5693fc7295a6fe76018bc86bc0192c6b1b4ac
Nataslamet <81040260+Nataslamet@users.noreply.github.com>
2022-06-15 23:37:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['628991544516', 'Valor_Bot', true]
+  ['628991544516', 'Nata', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = ['628991544516'] // Want some help?
